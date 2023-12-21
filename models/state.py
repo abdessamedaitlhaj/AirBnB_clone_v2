d8e5da2f9b00a092c84cad820f0afa14152e7343
“abdessamedaitlhaj” <aitlhaj.abdessamed@gmail.com>
2023-12-21 10:33:32 -0800

    airbnb clone sql

--- models/state.py
+++ models/state.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
 
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
+from sqlalchemy import Column, String
 
 class State(BaseModel):
     """ State class """
