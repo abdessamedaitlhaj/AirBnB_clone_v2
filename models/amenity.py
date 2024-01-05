31f8b990ed01536c1121818de00292589039aa05
“abdessamedaitlhaj” <aitlhaj.abdessamed@gmail.com>
2024-01-05 13:01:25 -0800

    airbnb sql

--- models/amenity.py
+++ models/amenity.py
@@ -9,3 +9,11 @@ from os import getenv
 class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        places = relationship(
+            "Place",
+            secondary="place_amenity",
+            back_populates="amenities",
+            viewonly=False,
+        )
